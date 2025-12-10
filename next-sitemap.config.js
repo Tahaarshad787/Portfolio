a11411ffb75c10974490662b9389ef55b734e73a
tahaarshad787 <tahaarshad311@gmail.com>
2025-12-10 19:48:48 +0500

    Fix: Prettier formatting

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || '',
+  siteUrl: process.env.SITE_URL || 'https://portfolio-tahaarshad.vercel.app',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   robotsTxtOptions: {
